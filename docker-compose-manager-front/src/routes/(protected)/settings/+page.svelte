b1aff4069dce6210ed32a25abf769e51dff4d356
Jordan Olivet <jordanolivet@gmail.com>
2026-03-13 23:28:02 +0100

    Add hour, minute and second to app update version

--- docker-compose-manager-front/src/routes/(protected)/settings/+page.svelte
+++ docker-compose-manager-front/src/routes/(protected)/settings/+page.svelte
@@ -111,7 +111,10 @@
       const day = date.getDate().toString().padStart(2, '0');
       const month = (date.getMonth() + 1).toString().padStart(2, '0');
       const year = date.getFullYear();
-      return `${day}/${month}/${year}`;
+      const hours = date.getHours().toString().padStart(2, '0');
+      const minutes = date.getMinutes().toString().padStart(2, '0');
+      const seconds = date.getSeconds().toString().padStart(2, '0');
+      return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
     } catch {
       return null;
     }
